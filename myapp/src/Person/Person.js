0bee34251ff641aa46c2271d907d3142a7553d9b
Tejas Pendse <tejas.pendse@hexagon.com>
2020-05-29 15:48:03 +0530

    React: lesson 36

--- myapp/src/Person/Person.js
+++ myapp/src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const person = () => {
-    return <h3 className="Person">I'm a new person!</h3>
+    const age = Math.floor(Math.random() * 30);
+    return <h3 className="Person">I'm a new person, and I am {age} years old!</h3>
 }
 
 export default person;
