6139bdc980b4d12333cdac714f0147cba234e5be
Tejas Pendse <tejas.pendse@hexagon.com>
2020-05-29 19:44:29 +0530

    added a button and a click handler

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,10 +5,14 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     people: [
-      { name: 'Tejas', age: 32, hobbies: 'riding a bike' },
-      { name: 'Neha', age: 30, hobbies: 'reading books' },
-      { name: 'Anay', age: 6 },
+      { name: 'Tejas',  age: 32, hobbies: 'riding a bike' },
+      { name: 'Neha',   age: 30, hobbies: 'reading books' },
+      { name: 'Anay',   age: 6                            },
     ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
   }
 
   render() {
@@ -16,9 +20,12 @@ class App extends Component {
       <div className="App">
         <h1>Hello, from JSX!</h1>
         <p>How're you doing?</p>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age}>My hobby is {this.state.people[0].hobbies} </Person>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My hobby is {this.state.people[1].hobbies} </Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age}>My hobby is {this.state.people[2].hobbies} </Person>
+
+        <button onClick={this.switchNameHandler}>Switch name</button>
+
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}>My hobby is {this.state.people[0].hobbies}</Person>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My hobby is {this.state.people[1].hobbies}</Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}>My hobby is {this.state.people[2].hobbies}</Person>
       </div>
     );
   }
