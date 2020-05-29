8585e090126d7997d03ffe774372f5ef9d71a248
Tejas Pendse <tejas.pendse@hexagon.com>
2020-05-29 19:27:44 +0530

    Outputting props children

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
       <div className="App">
         <h1>Hello, from JSX!</h1>
         <p>How're you doing?</p>
-        <Person name="Tejas" age="32" />
-        <Person name="Neha" age="30" />
+        <Person name="Tejas" age="32">My hobbies are riding a bike</Person>
+        <Person name="Neha" age="30">My hobbies are reading books</Person>
         <Person name="Anay" age="5" />
       </div>
     );
--- myapp/src/Person/Person.js
+++ myapp/src/Person/Person.js
@@ -4,7 +4,12 @@ const person = (props) => {
     const is_child = props.age < 18;
     const css_class = is_child ? "Child" : "Person";
     const label = is_child ? "Child" : "Adult";
-    return <h3 className={css_class}>I'm {props.name}, and I am {is_child ? "a" : "an"} {label}, {props.age} years old!</h3>
+    return (
+        <div>
+            <h3 className={css_class}>I'm {props.name}, and I am {is_child ? "a" : "an"} {label}, {props.age} years old!</h3>
+            <p className="Hobbies">{props.children}</p>
+        </div>
+    );
 }
 
 export default person;
