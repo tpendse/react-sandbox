f37fb9b5b3328db0ac4b4ca68171401a14534070
Tejas Pendse <tejas.pendse@hexagon.com>
2020-05-29 19:38:15 +0530

    introduced a state to App component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
-  render() 
-  {
+  state = {
+    people: [
+      { name: 'Tejas', age: 32, hobbies: 'riding a bike' },
+      { name: 'Neha', age: 30, hobbies: 'reading books' },
+      { name: 'Anay', age: 6 },
+    ]
+  }
+
+  render() {
     return (
       <div className="App">
         <h1>Hello, from JSX!</h1>
         <p>How're you doing?</p>
-        <Person name="Tejas" age="32">My hobbies are riding a bike</Person>
-        <Person name="Neha" age="30">My hobbies are reading books</Person>
-        <Person name="Anay" age="5" />
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}>My hobby is {this.state.people[0].hobbies} </Person>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My hobby is {this.state.people[1].hobbies} </Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}>My hobby is {this.state.people[2].hobbies} </Person>
       </div>
     );
   }
