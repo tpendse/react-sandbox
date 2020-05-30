e7ed9e6f50956ea9f3b9a44a64fa916e4e67f971
Tejas Pendse <tejas.pendse@hexagon.com>
2020-05-30 10:19:10 +0530

    react hooks trial - count increment isn't working for some readon

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  all_states = [
+const app = () => {
+  const all_states = [
       { name: 'Tejas',  age: 32, hobbies: 'riding a bike' },
       { name: 'Neha',   age: 30, hobbies: 'reading books' },
       { name: 'Anay',   age: 6                            },
     ];
 
-  default_state = { name: 'Tejas',  age: 32, hobbies: 'riding a bike' };
+  const default_state = { name: 'Tejas',  age: 32, hobbies: 'riding a bike' };
 
-  count = 0;
+  let count = 0;
 
-  state = this.default_state;
+//   state = this.default_state;
 
-  switchNameHandler = () => {
-    this.count += 1;
-    const new_state = this.all_states[this.count % 3];
-    this.setState(new_state);
+  const [personState, setStateDelegate] = useState(default_state);
+
+  const switchNameHandler = () => {
+    count += 1;
+    setStateDelegate(all_states[count % 3]);
+    console.log(count);
   }
 
-  render() {
     return (
       <div className="App">
         <h1>Hello, from JSX!</h1>
         <Person 
-          name={this.state.name} 
-          age={this.state.age}
-          clickHandler={this.switchNameHandler}>
-            My hobby is {this.state.hobbies}
+          name={personState.name}
+          age={personState.age}
+          clickHandler={switchNameHandler}>
+            My hobby is {personState.hobbies}!
           </Person>
 
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
